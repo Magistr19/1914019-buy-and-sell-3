79c930a36dbb4cd669d6be8aeb49f9d134dba683
Paiareli <sashapayareli@gmail.com>
2022-01-10 11:08:09 +0200

    module 2.13 done

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -8,6 +8,23 @@ const {HttpCode} = require(`../../constants`);
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
 
+const sendResponse = (res, statusCode, message) => {
+  const template = `
+    <!Doctype html>
+      <html lang="ru">
+      <head>
+        <title>With love from Node</title>
+      </head>
+      <body>${message}</body>
+    </html>`.trim();
+
+  res.writeHead(statusCode, {
+    'Content-Type': `text/html; charset=UTF-8`,
+  });
+
+  res.end(template);
+};
+
 const onClientConnect = async (req, res) => {
   const notFoundMessageText = `Not found`;
 
@@ -27,23 +44,6 @@ const onClientConnect = async (req, res) => {
       sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
       break;
   }
-};
-
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
 };
 
 module.exports = {
