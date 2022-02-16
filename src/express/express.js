db3680f10322e1fbc5805283e2296c759b96310d
Paiareli <sashapayareli@gmail.com>
2022-02-16 15:14:00 +0200

    express + pug templates

---
+++ src/express/express.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require(`express`);
+const path = require(`path`);
+
+// Маршруты приложения мы опишем в отдельных файлах.
+// Для определения маршрутов мы воспользуемся Router().
+// Примеры маршрутов будут продемонстрированы ниже по тексту.
+const offersRoutes = require(`./routes/offers-routes`);
+const myRoutes = require(`./routes/my-routes`);
+const mainRoutes = require(`./routes/main-routes`);
+
+// Зафиксируем порт для сервера
+const DEFAULT_PORT = 8080;
+
+const app = express();
+
+const PUBLIC_DIR = `public`;
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+// Подключим созданные маршруты
+app.use(`/offers`, offersRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/`, mainRoutes);
+
+// Запуск сервера
+app.listen(DEFAULT_PORT);
---
+++ src/express/routes/offers-routes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Подключаем и инициализируем экземпляр Router
+const {Router} = require(`express`);
+const offersRouter = new Router();
+
+// Определяем `GET` маршруты.
+// В качестве ответа отправляем путь маршрута.
+// Следует помнить, что в первом параметре мы указываем путь маршрута
+// без `offers`, т.к. уже указали при подключении модуля маршрута
+// в `express.js`.
+offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
+offersRouter.get(`/add`, (req, res) => res.render(`offers/new-ticket`));
+offersRouter.get(`/edit/:id`, (req, res) => res.render(`offers/ticket-edit`));
+offersRouter.get(`/:id`, (req, res) => res.render(`offers/ticket`));
+
+module.exports = offersRouter;
